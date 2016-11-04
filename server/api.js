fbaeecae4ea2b2abaff6fb6f37d99f8f46d388ff
Kat Guthrie <kathrynguthrie@gmail.com>
2016-11-04 15:45:58 -0400

    added postgres database boilerplate

--- server/api.js
+++ server/api.js
@@ -2,6 +2,7 @@
 
 const db = require('APP/db')
 const api = module.exports = require('express').Router()
+const pg = require('pg');
 
 api
   .get('/heartbeat', (req, res) => res.send({ok: true,}))
@@ -10,6 +11,18 @@ api
   .use('/celebs', require('./celebs'))
   .use('/reviews', require('./reviews'))
   .use('/products', require('./products'))
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 // Send along any errors
 api.use((err, req, res, next) => {
