880a53a3ea9009a0ed69a536d72c8e666c7b6a0f
Kat Guthrie <kathrynguthrie@gmail.com>
2016-11-04 15:51:59 -0400

    removed database boilerplate

--- server/api.js
+++ server/api.js
@@ -12,18 +12,6 @@ api
   .use('/reviews', require('./reviews'))
   .use('/products', require('./products'))
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
-
 // Send along any errors
 api.use((err, req, res, next) => {
   res.status(500).send(err)
